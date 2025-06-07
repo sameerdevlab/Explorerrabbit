a4df764df21825a4b682271c7a522f862655a685
Mohammed Sameer <123559014+sameerdevlab@users.noreply.github.com>
2025-06-07 18:21:52 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -121,7 +121,7 @@ const HomePage: React.FC = () => {
               </div>
               
               {/* Minimized input at the bottom */}
-              {mode === 'generate' ? (
+        {/* {mode === 'generate' ? (
                 <PromptInput minimized />
               ) : (
                 <TextInput minimized />
