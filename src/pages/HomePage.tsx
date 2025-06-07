cbc3204d19b8a1c03fe80286298e395d194b036d
Mohammed Sameer <123559014+sameerdevlab@users.noreply.github.com>
2025-06-07 18:24:43 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -81,7 +81,7 @@ const HomePage: React.FC = () => {
         
         <ModeToggle />
         
-        {/* <AnimatePresence mode="wait">
+        <AnimatePresence mode="wait">
           {!showResults ? (
             <motion.div
               key="input"
@@ -121,40 +121,14 @@ const HomePage: React.FC = () => {
               </div>
               
               {/* Minimized input at the bottom */}
-        {/* {mode === 'generate' ? (
+        {mode === 'generate' ? (
                 <PromptInput minimized />
               ) : (
                 <TextInput minimized />
               )}
             </motion.div>
           )}
-        </AnimatePresence> */}
-    {!showResults ? (
-  <>
-    {mode === 'generate' ? (<PromptInput />) : (<TextInput />)}
-  </>
-) : (
-  <>
-    <div className="flex flex-col md:flex-row gap-6">
-                <div className="w-full md:w-3/5">
-                  <ContentDisplay />
-                </div>
-                <div className="w-full md:w-2/5">
-                  <MCQDisplay />
-                </div>
-              </div>
-              
-              <div className="mt-8 text-center">
-                <button
-                  onClick={handleNewContent}
-                  className="text-purple-600 underline hover:text-purple-800 transition-colors"
-                >
-                  Start Over
-                </button>
-              </div>
-  </>
-)}
-
+        </AnimatePresence>
       </main>
     </div>
   );
