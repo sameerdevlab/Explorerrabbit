660e9914cf25951475fd74fd087efc71073fcfdc
Mohammed Sameer <123559014+sameerdevlab@users.noreply.github.com>
2025-06-08 17:03:35 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -96,7 +96,7 @@ const HomePage: React.FC = () => {
               </div>
               
               {/* MCQ and Social Media Post - Right side with combined scrolling */}
-              <div className="fixed w-full md:w-1/2 flex flex-col">
+              <div className="w-full md:w-1/2 flex flex-col">
                 <div className="flex-grow overflow-y-auto space-y-6">
                   <MCQDisplay />
                   <SocialMediaPostGenerator />
