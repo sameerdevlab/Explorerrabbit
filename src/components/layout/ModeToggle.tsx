56a186ab41c257a5a10f89c79b28680e105f33bd
Mohammed Sameer <123559014+sameerdevlab@users.noreply.github.com>
2025-06-10 21:17:44 +0530

    Updated ModeToggle.tsx

--- src/components/layout/ModeToggle.tsx
+++ src/components/layout/ModeToggle.tsx
@@ -20,7 +20,7 @@ const ModeToggle: React.FC = () => {
           
           {mode === 'generate' && (
             <motion.div
-              className="absolute inset-0 rounded-md shadow-lg"
+              className="absolute inset-0 rounded-md shadow-lg glowing-wrapper"
               layoutId="pill"
               initial={false}
               transition={{ type: "spring", duration: 0.5 }}
@@ -39,7 +39,7 @@ const ModeToggle: React.FC = () => {
           
           {mode === 'paste' && (
             <motion.div
-              className="absolute inset-0 rounded-md shadow-lg"
+              className="absolute inset-0 rounded-md shadow-lg glowing-wrapper"
               layoutId="pill"
               initial={false}
               transition={{ type: "spring", duration: 0.5 }}
