1ff80bd2caaf0917a1f01f314ff2dedb3f9be148
Mohammed Sameer <123559014+sameerdevlab@users.noreply.github.com>
2025-06-12 21:58:19 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -97,7 +97,7 @@ const HomePage: React.FC = () => {
               
               {/* MCQ and Social Media Post - Right side with sticky positioning */}
               <div className="sticky top-[104px] w-full md:w-1/2 flex flex-col h-[calc(100vh-136px)] z-10">
-                <div className="flex-grow overflow-y-auto space-y-6">
+                <div className="flex-grow overflow-y-auto space-y-6 pb-24">
                   <MCQDisplay />
                   <SocialMediaPostGenerator />
                 </div>
