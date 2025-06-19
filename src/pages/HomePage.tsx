357f3e640c9f6878e9dcc3211d3a1ed1c7f7a856
Mohammed Sameer <123559014+sameerdevlab@users.noreply.github.com>
2025-06-19 10:26:23 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -25,7 +25,7 @@ const HomePage: React.FC = () => {
     isGeneratingImages,
     isGeneratingMcqs,
     isProcessingPastedText,
-    loading: contentLoading
+    loading: contentLoading,
     saveContent,
     isSaving
   } = useContentStore();
