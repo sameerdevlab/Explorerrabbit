939c732f5a45e7824b5a892e85a499a69dab176a
Mohammed Sameer <123559014+sameerdevlab@users.noreply.github.com>
2025-06-22 11:54:15 +0530

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -21,7 +21,7 @@ const Header: React.FC = () => {
   
   return (
     <>
-      <header className="w-full sticky top-0 z-50 px-6 py-4 bg-gradient-to-r from-orange-500 via-yellow-500 to-orange-500 dark:from-slate-900 dark:via-slate-800 dark:to-slate-700 text-white shadow-lg border-b-2 border-white/20">
+      <header className="w-full sticky top-0 z-50 px-6 py-2 bg-gradient-to-r from-orange-500 via-yellow-500 to-orange-500 dark:from-slate-900 dark:via-slate-800 dark:to-slate-700 text-white shadow-lg border-b-2 border-white/20">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center gap-3">
           <div>
