7f4a802ba3c499f0446a0393f02a65dcd3803591
Mohammed Sameer <123559014+sameerdevlab@users.noreply.github.com>
2025-06-22 12:01:52 +0530

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -25,7 +25,7 @@ const Header: React.FC = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center gap-3">
           <div>
-            <img className="w-11 h-14" src="/rabbitLogoTr.png" atl="rabbit logo"/>
+            <img className="w-11 h-14" src="/rabbitLogoTr.png" atl="explorerrabbit logo"/>
           </div>
           <h1 className="text-2xl font-bold tracking-wide">EXPLORERrabbit</h1>
         </div>
