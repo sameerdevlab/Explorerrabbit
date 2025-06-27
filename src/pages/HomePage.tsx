6f0138484248fd494c5c0b75b96b0a144ad00e70
Mohammed Sameer <123559014+sameerdevlab@users.noreply.github.com>
2025-06-27 11:36:12 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -159,7 +159,7 @@ const HomePage: React.FC = () => {
           </motion.div>
         )}
 
-        <div className="mt-16">
+        <div className="mt-20">
           {/* Feature Highlights - Only show when not displaying results */}
           {!showResults && <FeatureHighlights />}
         </div>
